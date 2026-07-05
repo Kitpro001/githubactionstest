e98d40a1cf49e6615d09d208b23282cf6317e5fc
Kitpro001 <kitlearn402@gmail.com>
2026-07-05 12:06:44 +0800

    Initial commit

---
+++ src/test/java/com/example/githubactionstest/GithubactionstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
